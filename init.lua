9d7977bafde4b7f26a53d1e4b13def606ff852f9
Matthew Slauson <matthew@slauson.io>
2023-01-28 13:39:47 -0600

    add more plugins

--- init.lua
+++ init.lua
@@ -9,9 +9,3 @@ require('lualine').setup({
 
 require('nvim-tree').setup({})
 require('nvim-autopairs').setup({})
-nnoremap <leader>S <cmd>lua require('spectre').open()<CR>
-
-nnoremap <leader>sw <cmd>lua require('spectre').open_visual({select_word=true})<CR>
-vnoremap <leader>s <esc>:lua require('spectre').open_visual()<CR>
-nnoremap <leader>sp viw:lua require('spectre').open_file_search()<cr>
-
