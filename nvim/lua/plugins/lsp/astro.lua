509176aeabf32751e3a40174939ea2045be3a07e
Matthew Slauson <matthew@slauson.io>
2024-03-07 09:35:10 -0600

    add

---
+++ nvim/lua/plugins/lsp/astro.lua
@@ -0,0 +1,16 @@
+local utils = require("utils")
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "java" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "astro")
+    end,
+  },
+}
