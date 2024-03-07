d59ec1b207bfd062a04fc6ce03a43ca5f984acc1
Matthew Slauson <matthew@slauson.io>
2024-03-07 09:54:35 -0600

    fix

--- nvim/lua/plugins/lsp/astro.lua
+++ nvim/lua/plugins/lsp/astro.lua
@@ -7,10 +7,30 @@ return {
       vim.list_extend(opts.ensure_installed, { "astro" })
     end,
   },
+
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        astro = { "eslint_d", "prettierd" },
+      },
+    },
+  },
   {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
       opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, "astro")
     end,
   },
+  require("nvim-web-devicons").setup({
+    strict = true,
+    override_by_extension = {
+      astro = {
+        icon = "",
+        color = "#EF8547",
+        name = "astro",
+      },
+    },
+  }),
 }
