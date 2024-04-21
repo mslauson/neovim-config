7a8932d8e8b36dbf8a5c03889329d2137bcfa185
Matthew Slauson <matthew@slauson.io>
2024-04-21 01:26:52 -0500

    take out

--- nvim/lua/plugins/cmp/init.lua
+++ nvim/lua/plugins/cmp/init.lua
@@ -38,7 +38,6 @@ return {
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
       end
 
-      local luasnip = require("luasnip")
       local cmp = require("cmp")
 
       -- `/` cmdline setup.
@@ -66,9 +65,7 @@ return {
 
       ---@param opts cmp.ConfigSchema
       opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
-        { name = "luasnip" },
         { name = "emoji" },
-        { name = "orgmode" },
         { name = "zsh" },
 
         { name = "plugins" },
@@ -90,8 +87,8 @@ return {
             cmp.select_next_item()
             -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
             -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
+            -- elseif luasnip.expand_or_jumpable() then
+            -- luasnip.expand_or_jump()
           elseif has_words_before() then
             cmp.complete()
           else
@@ -101,8 +98,8 @@ return {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
+            -- elseif luasnip.jumpable(-1) then
+            -- luasnip.jump(-1)
           else
             fallback()
           end
