90b67050b7d11a272daa49b2bcf4f61c73d82f31
Matthew Slauson <matthew@slauson.io>
2024-07-29 21:26:28 -0500

    add packages

---
+++ versions/new-nvim/lua/plugins/cmp/packages.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "David-Kunz/cmp-npm",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        ft = "json",
+        config = function()
+          require("cmp-npm").setup({})
+        end,
+      },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      table.insert(opts.sources, { name = "npm", keyword_length = 4 })
+    end,
+  },
+}
