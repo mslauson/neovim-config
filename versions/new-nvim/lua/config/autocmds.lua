580ac3255529904a42fe8a8aae82c2dbb3ed2946
Matthew Slauson <matthew@slauson.io>
2024-07-30 11:25:59 -0500

    add save and git

--- versions/new-nvim/lua/config/autocmds.lua
+++ versions/new-nvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+local cmd = vim.api.nvim_create_user_command
+local namespace = vim.api.nvim_create_namespace
+
+autocmd({ "InsertLeave", "TextChanged" }, {
+  desc = "Save on Insert Leave",
+  pattern = "*.*",
+  callback = function()
+    vim.cmd.wa()
+  end,
+})
