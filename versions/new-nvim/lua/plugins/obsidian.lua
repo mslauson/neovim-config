d0c7a91ab68f69b904f26edf0dde64ffa20d46fa
Matthew Slauson <matthew@slauson.io>
2024-09-12 19:21:46 -0500

    ud

--- versions/new-nvim/lua/plugins/obsidian.lua
+++ versions/new-nvim/lua/plugins/obsidian.lua
@@ -19,10 +19,6 @@ return {
   opts = {
     workspaces = {
       {
-        name = "personal",
-        path = "~/vaults/personal",
-      },
-      {
         name = "work",
         path = "~/vaults/work",
       },
@@ -31,7 +27,25 @@ return {
       nvim_cmp = true,
       min_chars = 2,
     },
-
-    -- see below for full list of options 👇
+    notes_subdir = "notes",
+    daily_notes = {
+      -- Optional, if you keep daily notes in a separate directory.
+      folder = "notes/dailies",
+      -- Optional, if you want to change the date format for the ID of daily notes.
+      date_format = "%Y-%m-%d",
+      -- Optional, if you want to change the date format of the default alias of daily notes.
+      alias_format = "%B %-d, %Y",
+      -- Optional, default tags to add to each new daily note created.
+      default_tags = { "daily-notes" },
+      -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+      template = "daily.md",
+    },
+    templates = {
+      folder = "templates",
+      date_format = "%Y-%m-%d",
+      time_format = "%H:%M",
+      -- A map for custom variables, the key should be the variable and the value a function
+      substitutions = {},
+    },
   },
 }
