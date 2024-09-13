e4c30512f5361bd50bfc758ecdb2308d02a933f0
Matthew Slauson <matthew@slauson.io>
2024-09-13 15:20:58 -0500

    ud

--- versions/new-nvim/lua/plugins/utils/notes.lua
+++ versions/new-nvim/lua/plugins/utils/notes.lua
@@ -48,23 +48,6 @@ return {
       "3rd/image.nvim",
     },
     opts = { -- you can just pass {}, defaults below
-      renderer_options = {
-        mermaid = {
-          background = nil, -- nil | "transparent" | "white" | "#hex"
-          theme = nil, -- nil | "default" | "dark" | "forest" | "neutral"
-          scale = 1, -- nil | 1 (default) | 2  | 3 | ...
-        },
-        plantuml = {
-          charset = nil,
-        },
-        d2 = {
-          theme_id = nil,
-          dark_theme_id = nil,
-          scale = nil,
-          layout = nil,
-          sketch = nil,
-        },
-      },
     },
     config = function()
       require("diagram").setup({
@@ -74,7 +57,7 @@ return {
         },
         renderer_options = {
           mermaid = {
-            theme = "forest",
+            theme = "default",
           },
           plantuml = {
             charset = "utf-8",
