b1ac79411c6f754762e181da0ffc04817fa677e7
Matthew Slauson <matthew@slauson.io>
2024-09-13 09:38:12 -0500

    ud

--- versions/new-nvim/lua/plugins/utils/notes.lua
+++ versions/new-nvim/lua/plugins/utils/notes.lua
@@ -66,5 +66,24 @@ return {
         },
       },
     },
+    config = function()
+      require("diagram").setup({
+        integrations = {
+          require("diagram.integrations.markdown"),
+          require("diagram.integrations.neorg"),
+        },
+        renderer_options = {
+          mermaid = {
+            theme = "forest",
+          },
+          plantuml = {
+            charset = "utf-8",
+          },
+          d2 = {
+            theme_id = 1,
+          },
+        },
+      })
+    end,
   },
 }
