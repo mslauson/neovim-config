e4e9e8e907a116319d5db5ec97ce15167c1415ba
Matthew Slauson <matthew@slauson.io>
2024-09-12 20:32:08 -0500

    ud

--- versions/new-nvim/lua/plugins/utils/notes.lua
+++ versions/new-nvim/lua/plugins/utils/notes.lua
@@ -1,3 +1,6 @@
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua"
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua"
+
 return {
   {
     "3rd/image.nvim",
