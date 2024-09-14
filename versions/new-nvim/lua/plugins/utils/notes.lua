fd22599015b7a8550a555b415432d8c4e099ff82
Matthew Slauson <matthew@slauson.io>
2024-09-14 16:45:11 -0500

    ud

--- versions/new-nvim/lua/plugins/utils/notes.lua
+++ versions/new-nvim/lua/plugins/utils/notes.lua
@@ -8,12 +8,13 @@ return {
       -- default config
       require("image").setup({
         backend = "kitty",
+        kitty_method = "normal",
         integrations = {
           markdown = {
             enabled = true,
             clear_in_insert_mode = false,
             download_remote_images = true,
-            only_render_image_at_cursor = false,
+            only_render_image_at_cursor = true,
             filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
           },
           neorg = {
@@ -44,7 +45,7 @@ return {
   },
   {
     "3rd/diagram.nvim",
-    enabled = false,
+    enabled = true,
     dependencies = {
       "3rd/image.nvim",
     },
